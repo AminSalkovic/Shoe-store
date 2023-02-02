d7b40698a077cfca3a7437807321ec3f3fa03f08
AminSalkovic <aminsalkovic2019@gmail.com>
2023-02-02 20:54:31 +0100

    add

--- src/components/ArticleCardInformation.tsx
+++ src/components/ArticleCardInformation.tsx
@@ -2,6 +2,7 @@ import * as AiIcons from 'react-icons/ai'
 import './ArticleCardInformation.css'
 import { Button } from 'react-bootstrap'
 import { useShoppingCart } from '../context/ShoppingCartContext'
+import 'animate.css';
 
 type articleCardInfromationProps={
   id:number,
@@ -16,6 +17,18 @@ const ArticleCardInformation = ({heading,imgUrl,password,price,id,turnOff}:artic
 
   const{increaseCartQuantity}=useShoppingCart()
 
+  const number46=46;
+
+  const createDigits=()=>{
+    const digits=[];
+    for(let i=40;i<49;i++){
+      digits.push(
+        <button  className={`${i > number46 ? 'numberButtonDontHave' : 'numberButtonHave'}`} key={i}>{i}</button>
+      )
+    }
+    return digits;
+  }
+
   return (
     <div className='articleCardInformation'>
       <div className='closeIcon' onClick={turnOff}>
@@ -24,13 +37,18 @@ const ArticleCardInformation = ({heading,imgUrl,password,price,id,turnOff}:artic
       <div style={{display:'flex'}}>
           <div className='articleCardInformationPart1'>
             <img src={imgUrl}/>
-            <Button onClick={()=>increaseCartQuantity(id)}>Dodaj u korpu</Button>
+            <Button style={{marginTop:'30px'}} variant='secondary' onClick={()=>increaseCartQuantity(id)}>
+              Dodaj u korpu <AiIcons.AiOutlineShoppingCart style={{fontSize:'20px'}}/>
+            </Button>
           </div>
           <div className='articleCardInformationPart2'>
-              <h1>{heading}</h1>
-              <p>Br artikla: {password}</p>
+               <h1>{heading}</h1>
+               <p>Br artikla: {password}</p>
               <h2>Cena:{price}RSD</h2>
                 <p>Cena je informativna i <br/> moze se  razlikovati <br/> od cene u prodavnici</p>
+                <div className="buttons">
+                  {createDigits()}
+                </div>
           </div>
       </div>
     </div>
